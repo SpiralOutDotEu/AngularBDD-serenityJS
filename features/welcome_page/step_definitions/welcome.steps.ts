a8856a4001eb992c6fef89ac95bfb99ebb5c0056
Spiral Out <spiralout.eu@gmail.com>
2017-08-27 17:30:38 +0300

    implement steps for the scenario of displaying welcome message

---
+++ features/welcome_page/step_definitions/welcome.steps.ts
@@ -0,0 +1,20 @@
+import {browser, by, element} from "protractor";
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+export = function myStepDefinitions () {
+
+  this.Given(/^I am on the homepage$/, function (callback) {
+    browser.get("/").then(callback());
+  });
+
+
+  this.Then(/^I should see welcome message$/, function (callback) {
+    let el = element(by.css('app-root h1')).getText();
+    expect(el).to.eventually.equal("Welcome to app!!").then(callback());
+  });
+
+};
